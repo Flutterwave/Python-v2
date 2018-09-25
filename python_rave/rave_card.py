0c768441e1d90684af4b75516d1a9321978948a0
Ifunanya Ikemma <ifavour902@gmail.com>
2018-09-25 18:33:05 +0100

    Resolved Response data in Rave SDK

--- python_rave/rave_card.py
+++ python_rave/rave_card.py
@@ -1,7 +1,6 @@
 from python_rave.rave_exceptions import RaveError, IncompletePaymentDetailsError, CardChargeError, TransactionVerificationError, ServerError
 from python_rave.rave_payment import Payment
 from python_rave.rave_misc import generateTransactionReference
-
 class Card(Payment):
     """ This is the rave object for card transactions. It contains the following public functions:\n
         .charge -- This is for making a card charge\n
@@ -10,8 +9,6 @@ class Card(Payment):
     """
     def __init__(self, publicKey, secretKey, production, usingEnv):
         super(Card, self).__init__(publicKey, secretKey, production, usingEnv)
-
-
     # returns true if further action is required, false if it isn't    
     def _handleChargeResponse(self, response, txRef, request=None):
         """ This handles charge responses """
@@ -19,13 +16,11 @@ class Card(Payment):
         
         responseJson = res["json"]
         flwRef = res["flwRef"]
-
         # Checking if there is auth url
         if responseJson["data"].get("authurl", "N/A") == "N/A":
             authUrl = None
         else:
             authUrl = responseJson["data"]["authurl"]
-
         # If all preliminary checks passed
         if not (responseJson["data"].get("chargeResponseCode", None) == "00"):
             # Otherwise we return that further action is required, along with the response
@@ -33,9 +28,7 @@ class Card(Payment):
             return {"error": False,  "validationRequired": True, "txRef": txRef, "flwRef": flwRef, "suggestedAuth": suggestedAuth, "authUrl": authUrl}
         else:
             return {"error": False,  "validationRequired": False, "txRef": txRef, "flwRef": flwRef, "suggestedAuth": None, "authUrl": authUrl}
-
     
-
     # This can be altered by implementing classes but this is the default behaviour
     # Returns True and the data if successful
     def _handleVerifyResponse(self, response, txRef):
@@ -43,13 +36,16 @@ class Card(Payment):
              Parameters include:\n
             response (dict) -- This is the response Http object returned from the verify call
          """
-
         # Checking if there was a server error during the call (in this case html is returned instead of json)
         res =  self._preliminaryResponseChecks(response, TransactionVerificationError, txRef=txRef)
-
+        # sets flwref, amount, chargedAmount, status, chargeCode, responseMessage as variables from the verification response
         responseJson = res["json"]
-        flwRef = res["flwRef"]
-
+        flwRef = responseJson["data"]["flwref"]
+        amount = responseJson["data"]["amount"]
+        chargedAmount = responseJson["data"]["chargedamount"]
+        status = responseJson["data"]["status"]
+        chargeCode = responseJson["data"]["chargecode"]
+        vbvmessage = responseJson["data"]["vbvmessage"]
         # Check if the call returned something other than a 200
         if not response.ok:
             errMsg = responseJson["data"].get("message", "Your call failed with no response")
@@ -57,11 +53,10 @@ class Card(Payment):
         
         # if the chargecode is not 00
         elif not (responseJson["data"].get("chargecode", None) == "00"):
-            return {"error": False, "transactionComplete": False, "txRef": txRef, "flwRef":flwRef, "cardToken": responseJson["data"]["card"]["card_tokens"][0]["embedtoken"]}
+            return {"status": status, "error": False, "transactionComplete": False, "amount": amount, "chargedAmount": chargedAmount, "chargecode": chargeCode, "vbvmessage": vbvmessage, "txRef": txRef, "flwRef": flwRef, "cardToken": responseJson["data"]["card"]["card_tokens"][0]["embedtoken"]}
         
         else:
-            return {"error":False, "transactionComplete": True, "txRef": txRef, "flwRef": flwRef, "cardToken": responseJson["data"]["card"]["card_tokens"][0]["embedtoken"]}
-
+            return {"status": status, "error": False, "transactionComplete": True, "amount": amount, "chargedAmount": chargedAmount, "chargecode": chargeCode, "vbvmessage": vbvmessage, "txRef": txRef, "flwRef": flwRef, "cardToken": responseJson["data"]["card"]["card_tokens"][0]["embedtoken"]}
     
     # Charge card function
     def charge(self, cardDetails, hasFailed=False, chargeWithToken=False):
@@ -70,10 +65,8 @@ class Card(Payment):
             cardDetails (dict) -- This is a dictionary comprising payload parameters.\n
             hasFailed (bool) -- This indicates whether the request had previously failed for timeout handling
         """
-
         # Checking for required card components
         requiredParameters = ["cardno", "cvv", "expirymonth", "expiryyear", "amount", "email", "phonenumber", "firstname", "lastname", "IP"]
-
         # setting the endpoint
         if not chargeWithToken:
             endpoint = self._baseUrl + self._endpointMap["card"]["charge"]
@@ -81,24 +74,14 @@ class Card(Payment):
             endpoint = self._baseUrl + self._endpointMap["card"]["preauthSavedCard"]
             # add token to requiredParameters
             requiredParameters.append("token")
-
         if not ("txRef" in cardDetails):
             cardDetails.update({"txRef":generateTransactionReference()})
-
         
         return super(Card, self).charge(cardDetails, requiredParameters, endpoint)
     
-
     def validate(self, flwRef, otp):
         endpoint = self._baseUrl + self._endpointMap["card"]["validate"]
         return super(Card, self).validate(flwRef, otp, endpoint)
-
     def verify(self, txRef):
         endpoint = self._baseUrl + self._endpointMap["card"]["verify"]
         return super(Card, self).verify(txRef, endpoint)
-
-
-
-        
-
-
