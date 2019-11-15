574738044cc3edfc3894b1f7a09c4d57e7089af8
Cornelius Ashley <korneliosyaovi@gmail.com>
2019-11-15 16:56:15 +0100

    Updated Virtual card and account superclasses
    Reviewed codebase and simplified function names in the superclasses.

--- rave_python/rave_virtualcard.py
+++ rave_python/rave_virtualcard.py
@@ -56,9 +56,10 @@ class VirtualCard(RaveBase):
             raise CardStatusError(type, {"error": True, "returnedData": responseJson })
 
 
+
     #function to create a virtual card 
-    #Params: cardDetails - a dict containing currency, amount, billing_name, billing_address, billing_city, billing_state, billing_postal_code, billing_country
-    def Create(self, vcardDetails):
+    #Params: vcardDetails - a dict containing currency, amount, billing_name, billing_address, billing_city, billing_state, billing_postal_code, billing_country
+    def create(self, vcardDetails):
         vcardDetails = copy.copy(vcardDetails)
         vcardDetails.update({"seckey": self._getSecretKey()})
         
@@ -70,39 +71,39 @@ class VirtualCard(RaveBase):
         return self._handleCreateResponse(response, vcardDetails)
 
     #gets all virtual cards connected to a merchant's account
-    def allCards(self):
+    def all(self):
         endpoint = self._baseUrl + self._endpointMap["virtual_card"]["list"] + "?seckey="+ self._getSecretKey()
         data = {"seckey": self._getSecretKey()}
         return self._handleCardStatusRequests("List", endpoint, isPostRequest=True, data=data)
 
     #permanently deletes a card with specified id 
-    def cancelCard(self, card_id):
-        if not id:
+    def cancel(self, card_id):
+        if not card_id:
             return "Card id was not supplied. Kindly supply one"
         endpoint = self._baseUrl + self._endpointMap["virtual_card"]["terminate"] + str(card_id) + "/terminate"
         data = {"seckey": self._getSecretKey()}
         return self._handleCardStatusRequests("Cancel", endpoint, isPostRequest=True, data=data)
 
     #fetches Card details and transactions for a cars with specified id
-    def getCard(self, card_id):
+    def get(self, card_id):
         endpoint = self._baseUrl + self._endpointMap["virtual_card"]["get"]
         data = {"seckey": self._getSecretKey()}
         return self._handleCardStatusRequests("Get", endpoint, isPostRequest=True, data=data)
 
     #temporarily suspends the use of card
-    def freezeCard(self, card_id):
+    def freeze(self, card_id):
         endpoint = self._baseUrl + self._endpointMap["virtual_card"]["freeze"] + str(card_id) + "/status/block"
         data = {"seckey": self._getSecretKey()}
         return self._handleCardStatusRequests("Freeze", endpoint, isPostRequest=True, data=data)
 
     #reverses the freeze card operation
-    def unfreezeCard(self, card_id):
+    def unfreeze(self, card_id):
         endpoint = self._baseUrl + self._endpointMap["virtual_card"]["unfreeze"] + str(card_id) + "/status/unblock"
         data = {"seckey": self._getSecretKey()}
         return self._handleCardStatusRequests("Unfreeze", endpoint, isPostRequest=True, data=data)
 
     #funds a card with specified balance for online transactions
-    def fundCard(self, card_id, amount, currency):
+    def fund(self, card_id, amount, currency):
         data = {
             "card_id": card_id,
             "amount" : amount,
@@ -113,7 +114,7 @@ class VirtualCard(RaveBase):
         return self._handleCardStatusRequests("Fund", endpoint, isPostRequest=True, data=data)
 
     #withdraws funds from Virtual card. Withdrawn funds are added to Rave Balance
-    def Withdraw(self, card_id, amount):
+    def withdraw(self, card_id, amount):
         data = {
             "card_id": card_id,
             "amount" : amount,
