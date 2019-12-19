de9c3fa5eceb8d7dfe67eca330265c99d9d6c9ba
Cornelius Ashley <korneliosyaovi@gmail.com>
2019-12-19 16:01:45 +0100

    Updated Mpesa Superclass

--- rave_python/rave_mpesa.py
+++ rave_python/rave_mpesa.py
@@ -1,3 +1,5 @@
+from rave_python.rave_exceptions import RaveError, IncompletePaymentDetailsError, AccountChargeError, TransactionVerificationError, TransactionValidationError, ServerError
+
 from rave_python.rave_payment import Payment
 from rave_python.rave_misc import generateTransactionReference
 import json
@@ -17,7 +19,7 @@ class Mpesa(Payment):
         # Setting the endpoint
         endpoint = self._baseUrl + self._endpointMap["account"]["charge"]
         # Adding boilerplate mpesa requirements
-        accountDetails.update({"payment_type": "mpesa", "country":"KE", "is_mpesa":"1", "currency":"KES"})
+        accountDetails.update({"payment_type": "mpesa", "country":"KE", "is_mpesa":"1", "is_mpesa_lipa":"1", "currency":"KES"})
         # If transaction reference is not set 
         if not ("txRef" in accountDetails):
             accountDetails.update({"txRef": generateTransactionReference()})
@@ -29,3 +31,7 @@ class Mpesa(Payment):
         requiredParameters = ["amount", "email", "phonenumber", "IP"]
         res = super(Mpesa, self).charge(accountDetails, requiredParameters, endpoint, isMpesa=True)
         return res
+
+    def verify(self, txRef):
+        endpoint = self._baseUrl + self._endpointMap["account"]["verify"]
+        return super(Mpesa, self).verify(txRef, endpoint)
