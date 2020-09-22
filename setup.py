cfbb750eb64b749233ebc949d9a5160df18774e4
Cornelius Ashley-Osuzoka <korneliosyaovi@gmail.com>
2020-09-23 00:01:58 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rave_python",
-    version="1.2.2",
+    version="1.2.4",
     author="Flutterwave",
     author_email="developers@flutterwavego.com",
     description="Official Rave Python Wrapper By Flutterwave",
